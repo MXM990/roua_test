038677b34b50bdf737eb484012d10afea48d0b69
MXM <morerise@gmail.com>
2026-05-25 18:32:12 +0300

    Add project files.

---
+++ ConsoleApp212/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using res_user;
+
+namespace ConsoleApp212
+{
+
+    interface IEmployeeActions
+    {
+        void AddMeal();
+        void ViewMeals();
+        void OrderMeal();
+        void ViewBills();
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Manager manager = new Manager("1", "1");
+            Report rrr = new Report("2", "2");
+            while (true)
+            {
+                Console.Clear();
+                Console.WriteLine("=== Restaurant Management System ===");
+                Console.Write("Username: ");
+                string username = Console.ReadLine();
+                Console.Write("Password: ");
+                string password = Console.ReadLine();
+
+                User currentUser = null;
+
+
+                if (username == manager.Username && password == manager.Password)
+                {
+                    currentUser = manager;
+                }
+                else if (username == rrr.Username && password == rrr.Password)
+                {
+                    currentUser = rrr;
+                }
+                else
+                {
+                    currentUser = manager.FindEmployee(username, password);
+                }
+
+
+                if (currentUser == null)
+                {
+                    Console.WriteLine("Invalid credentials. Press any key to try again...");
+                    Console.ReadKey();
+                    continue;
+                }
+
+                Console.WriteLine($"Welcome, {currentUser.GetRole()}!");
+                currentUser.ShowMenu();
+            }
+        }
+    }
+}
---
+++ ConsoleApp212/Report.cs
@@ -0,0 +1,55 @@
+﻿using res_user;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp212
+{
+    internal class Report : User
+    {
+        public Report(string username, string password) : base(username, password)
+        {
+        }
+
+        public override string GetRole()
+        {
+            return "Report";
+        }
+        public override void ShowMenu()
+        {
+            while (true)
+            {
+                Console.WriteLine("\n--- report Menu ---");
+                Console.WriteLine("1. show report");
+                Console.WriteLine("2. Logout");
+                Console.Write("Enter your choice: ");
+                string choice = Console.ReadLine();
+
+                if (choice == "1")
+                {
+                    read();
+                }
+                else if (choice == "2")
+                {
+                    Console.WriteLine("Logging out...");
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("Invalid choice, please try again.");
+                }
+            }
+        }
+        public void read ()
+        {
+          string [] a =   File.ReadAllLines("mxm.txt");
+            foreach (var item in a)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
